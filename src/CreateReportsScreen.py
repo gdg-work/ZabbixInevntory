5f5972811ebe01b7583d0c2b9a466486ebdd19d5
Dmitriy G. Golub <d.golub@hostco.ru>
2016-10-27 20:11:43 +0500

    Fixed differential report field in Properties application and item names

--- src/CreateReportsScreen.py
+++ src/CreateReportsScreen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf8 -*-
 """
-A small program for creating an application in the host with 4 items:
+A small program for creating an application in the host with several items like:
     - host name
     - host accounting number (инвентарный номер) from host.inventory
     - host S/N (from inventory)
@@ -17,9 +17,9 @@ from urllib.parse import quote
 import subprocess as sp
 import logging
 import logging.config
-import locale
 import yaml
 from i18n import _
+import locale
 
 #
 # Make you configuration here, but don't forget to backup the file first
@@ -27,27 +27,30 @@ from i18n import _
 dConfig = yaml.load("""
     version: 1
     ZabbixAPI:
-        ip: 127.0.0.1
+        ip: zabbix.protek.ru
         user: zabbix
         password: A3hHr88man01
     ZabbixSender:
         ip: 127.0.0.1
         port: 10051
-        # port: 8951
     Jasper:
         # http://10.1.96.163:8080/jasperserver/flow.html?_flowId=viewReportFlow&\
         #       ParentFolderUri=%2Freports&j_username=zabbix&j_password=Z@bbix123&\
         #       reportUnit=%2Freports%2FHOST_ITEMS&HOST_NAME=HOST-EVA-4400
-        ip: 10.1.96.163
+        ip: zabbix.protek.ru
         port: 8080
         root: "/jasperserver"
         report_folder: "reports"
         user: zabbix
         password: "Z@bbix123"
-        report_name: HOST_ITEMS
-        flow_id: viewReportFlow
-        report_unit: "/reports/HOST_ITEMS"
         host_name: "HOST_NAME={}"
+        flow_id: viewReportFlow
+        CFG_Report:
+            report_name: host_by_name
+            report_unit: "/reports/host_by_name"
+        DIFF_Report:
+            report_name: host_changes_by_date
+            report_unit: "/reports/host_changes_by_date"
     Logs:
         version: 1
         formatters:
@@ -91,6 +94,25 @@ ZABBIX_SENDER = '/usr/bin/zabbix_sender'
 ZBX_SENDER_TIMEOUT = 120
 
 
+def s_make_url(d_config, sRepName, s_host_name):
+    """dCfg is a dictionary of configuration data for Jasper URL"""
+    if 'flow_id' in d_config:
+        s_flow = "flow.html?_flowId={}".format(d_config['flow_id'])
+        s_base = 'http://{0}:{1}/{2}/{3}'.format(d_config['ip'], d_config['port'], d_config['root'], s_flow)
+        s_base = re.sub(r'//+', '/', s_base)
+        l_parts = ['ParentFolderUri=/{0}'.format(quote(d_config['report_folder']))]
+        l_parts.append('j_username={}'.format(quote(d_config['user'])))
+        l_parts.append('j_password={}'.format(quote(d_config['password'])))
+        l_parts.append('reportUnit={}'.format(quote(d_config[sRepName]['report_unit'])))
+        l_parts.append('HOST_NAME={}'.format(s_host_name))
+        s_parts = '&'.join([(s) for s in l_parts])
+        s_url = s_base + '&' + s_parts
+        s_url = s_url
+    else:
+        raise(Exception)
+    return(s_url)
+
+
 class InventoryHost:
     def __init__(self, s_name, s_desc, s_ip, s_inv, s_sn, s_site):
         """constructor of an object, parameters:
@@ -107,22 +129,12 @@ class InventoryHost:
         self.s_site = s_site
         self.s_netaccess = s_ip
         self.s_url = ''
+        self.d_config = {}
         return
 
-    def s_make_url(self, dCfg):
-        """dCfg is a dictionary of configuration data for Jasper URL"""
-        s_flow = "flow.html?_flowId={}".format(dCfg['flow_id'])
-        s_base = 'http://{0}:{1}/{2}/{3}'.format(dCfg['ip'], dCfg['port'], dCfg['root'], s_flow)
-        s_base = re.sub(r'//+', '/', s_base)
-        l_parts = ['ParentFolderUri=/{0}'.format(quote(dCfg['report_folder']))]
-        l_parts.append('j_username={}'.format(quote(dCfg['user'])))
-        l_parts.append('j_password={}'.format(quote(dCfg['password'])))
-        l_parts.append('reportUnit={}'.format(quote(dCfg['report_unit'])))
-        l_parts.append('HOST_NAME={}'.format(self.s_name))
-        s_parts = '&'.join([(s) for s in l_parts])
-        s_url = s_base + '&' + s_parts
-        self.s_url = s_url
-        return(s_url)
+    def set_cfg(self, dCfg):
+        self.d_config = dCfg
+        return
 
     def make_app_items(self, o_api, o_sender):
         APP_NAME = 'Properties'
@@ -138,38 +150,46 @@ class InventoryHost:
             #                                     'value_type': 1})
             # o_sender.add_item_value(oNameItem, self.s_name)
             oDescItem = o_zbx_host._oAddItem(
-                'Description', APP_NAME, dParams={'key': zi._sMkKey(APP_NAME, 'Description'),
+                '1_Description', APP_NAME, dParams={'key': zi._sMkKey(APP_NAME, 'Description'),
                                                   'description': _('Short description'),
                                                   'value_type': 1})
             o_sender.add_item_value(oDescItem, self.s_desc)
             oIP_Item = o_zbx_host._oAddItem(
-                'Controlling IP', APP_NAME,
+                '2_Controlling IP', APP_NAME,
                 dParams={'key': zi._sMkKey(APP_NAME, 'IP'),
                          'description': _('Internet Protocol address'),
                          'value_type': 1})
             o_sender.add_item_value(oIP_Item, self.s_netaccess)
             oInvItem = o_zbx_host._oAddItem(
-                'Inventory #', APP_NAME,
+                '3_Inventory #', APP_NAME,
                 dParams={'key': zi._sMkKey(APP_NAME, 'Inventory Number'),
                          'description': _('Asset tag'),
                          'value_type': 1})
             o_sender.add_item_value(oInvItem, self.s_inv)
-            oSN_Item = o_zbx_host._oAddItem('Serial Number', APP_NAME,
+            oSN_Item = o_zbx_host._oAddItem('4_Serial Number', APP_NAME,
                 dParams={'value_type': 1,
                          'description': _('System Serial Number'),
                          'key': zi._sMkKey(APP_NAME, 'Serial Num')})
             if oSN_Item:
                 o_sender.add_item_value(oSN_Item, self.s_sn)
-            oSiteItem = o_zbx_host._oAddItem('Site address', APP_NAME,
+            oSiteItem = o_zbx_host._oAddItem('5_Site address', APP_NAME,
                 dParams={'value_type': 1,
                          'description': _('Physical placement'),
                          'key': zi._sMkKey(APP_NAME, 'Site')})
             o_sender.add_item_value(oSiteItem, self.s_site)
-            oRepItem = o_zbx_host._oAddItem('Report URL', APP_NAME,
-                dParams={'value_type': 1,
-                         'description': _('Configuration report URL'),
-                         'key': zi._sMkKey(APP_NAME, 'URL')})
-            o_sender.add_item_value(oRepItem, self.s_url)
+
+            s_cfg_url = s_make_url(self.d_config['Jasper'], 'CFG_Report', self.s_name)
+            oCfgRepItem = o_zbx_host._oAddItem('6_CFG Report URL', APP_NAME,
+                    dParams={'value_type': 1,
+                    'description': _('Configuration report URL'),
+                    'key': zi._sMkKey(APP_NAME, 'CFG Rep URL')})
+            o_sender.add_item_value(oCfgRepItem, s_cfg_url)
+
+            s_diff_url = s_make_url(self.d_config['Jasper'], 'DIFF_Report', self.s_name)
+            oDiffRepItem = o_zbx_host._oAddItem('7_DIFF Report URL', APP_NAME, dParams={'value_type': 1,
+                    'description': _('Differential report URL'),
+                    'key': zi._sMkKey(APP_NAME, 'DIFF Rep URL')})
+            o_sender.add_item_value(oDiffRepItem, s_diff_url)
         except zi.MyZabbixException as e:
             oLog.error('Error communicating with Zabbix, host is ' + self.s_name)
         return
@@ -207,20 +227,23 @@ class BufferedSender:
 
     def send_values(self):
         l_args = [ZABBIX_SENDER, '-z', self.s_zbx_srv, '-p', str(self.i_zbx_port), '-i', '-']
-        o_proc = sp.Popen(l_args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+        o_proc = sp.Popen(l_args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=False)
         s_data = '\n'.join(self.ls_sendlist)
-        oLog.debug('*DBG* Data to send:')
-        oLog.debug('*CONT*' + s_data)
+        b_data = s_data.encode(ZABBIX_ENCODING)
+        # oLog.debug('*DBG* Data to send:')
+        # oLog.debug('*CONT*' + s_data)
+        with open('/tmp/zbx_send.bin', 'wb') as f_out:
+            f_out.write(b_data)
         try:
             locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
-            (s_stdout, s_stderr) = o_proc.communicate(s_data, timeout=ZBX_SENDER_TIMEOUT)
+            (b_stdout, b_stderr) = o_proc.communicate(b_data, timeout=ZBX_SENDER_TIMEOUT)
         except sp.TimeoutExpired as e:
             o_proc.kill()
             oLog.error('Error calling zabbix_sender, Timeout expired')
         if o_proc.returncode != 0:
             oLog.error('*ERR* Non-zero return code from subprocess\n' +
-                       '*        stdout is ' +  s_stdout + '\n' +
-                       '*        stderr is ' + s_stderr)
+                       '*        stdout is ' +  b_stdout.decode(ZABBIX_ENCODING) + '\n' +
+                       '*        stderr is ' + b_stderr.decode(ZABBIX_ENCODING))
         return
 
 
@@ -251,7 +274,6 @@ def fi_do_the_work():
         # sName = dHost['name']
         sIP = 'IP.AD.RE.SS'
         sName = dHost['host']
-        sDesc = dHost['description']
         lInterfaces = dHost['interfaces']
         for dInt in lInterfaces:
             if dInt['default'] == '1':
@@ -260,9 +282,10 @@ def fi_do_the_work():
                 else:
                     sIP = dInt['dns']
                 break
-        oLog.debug('Host is {} ({}) at IP'.format(sName, sDesc, sIP))
         dInv = dHost.get('inventory', {})
         if len(dInv) > 0:
+            sDesc = dInv['type_full']
+            oLog.debug('Host is {} ({}) at IP'.format(sName, sDesc, sIP))
             sInvNo = dInv['asset_tag']
             if sInvNo != '':
                 sSN = dInv['serialno_a']
@@ -272,7 +295,7 @@ def fi_do_the_work():
                     dInv['site_notes']]
                 sAddr = ', '.join([s for s in lAddrList if s != ''])
                 o_inv_host = InventoryHost(sName, sDesc, sIP, sInvNo, sSN, sAddr)
-                o_inv_host.s_make_url(dConfig['Jasper'])
+                o_inv_host.set_cfg(dConfig)
                 o_inv_host.make_app_items(oAPI, oNewSend)
                 lHostData.append(o_inv_host)
     oNewSend.send_values()
@@ -280,6 +303,7 @@ def fi_do_the_work():
 
 if __name__ == '__main__':
     # main section
+    # locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
     oLog = logging.getLogger('Create_Properties_Screen')
     logging.config.dictConfig(dConfig['Logs'])
     iRetCode = fi_do_the_work()
